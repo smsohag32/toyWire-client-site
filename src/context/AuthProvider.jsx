c7bdff6fef35e14fa41654188c1dbf2c73303292
SM Sohag <sohagsheik32@gmail.com>
2023-05-18 11:28:32 +0600

    auth context created and firebase user authentication implemented

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,70 @@
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { createContext, useEffect, useState } from "react";
+
+const auth = getAuth(app);
+
+// google sing in provider
+
+const googleProvider = new GoogleAuthProvider();
+
+// auth context
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  //   user sing up
+  const userSingUp = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //   user log in
+  const userLogin = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  //   user google log in
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const userLogOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  //   observer
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (loggedUser) => {
+      setUser(loggedUser);
+      setLoading(false);
+    });
+    return () => unsub();
+  }, []);
+
+  // context value object
+  const authInfo = {
+    user,
+    loading,
+    userSingUp,
+    userLogin,
+    googleLogin,
+    userLogOut,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
+import AuthProvider from "./context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={routes}></RouterProvider>
+  <AuthProvider>
+    <RouterProvider router={routes}></RouterProvider>
+  </AuthProvider>
 );
