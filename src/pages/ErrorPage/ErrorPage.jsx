6412bd8f487321a2af29dd46802945fd0778d5d5
SM Sohag <sohagsheik32@gmail.com>
2023-05-19 22:54:03 +0600

    error page done

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,18 +1,37 @@
 import { Link, useRouteError } from "react-router-dom";
-
+import { useLottie } from "lottie-react";
+import errorImg from "../../assets/animation/errorImg.json";
+import {
+  AiFillLeftSquare,
+  AiOutlineLeftCircle,
+  AiOutlineLeftSquare,
+} from "react-icons/ai";
 const ErrorPage = () => {
   const { error, status } = useRouteError();
 
   console.log(error, status);
+
+  const options = {
+    animationData: errorImg,
+    loop: true,
+  };
+
+  const { View } = useLottie(options);
+
   return (
-    <div className="h-screen w-full flex items-start justify-center">
-      <div>
-        <div></div>
-        <div>
-          {error ? <p>{error.message}</p> : <p>Something Went Wrong !</p>}
-          <Link className="primary-btn" to="/">
-            Home Page
-          </Link>
+    <div className="h-screen bg-slate-100 bg-opacity-70 w-full text-center flex items-center justify-center">
+      <div className="w">
+        <div className="w-1/2 mx-auto">{View}</div>
+        <div className="">
+          <div className="mb-7 text-sm pt-5 opacity-70 mt-5">
+            {error ? <p>{error.message}</p> : <p>Something Went Wrong !</p>}
+          </div>
+          <div className="flex items-center gap-2 justify-center">
+            <Link className="" to="/">
+              <AiOutlineLeftCircle className="text-3xl btn btn-outline btn-circle primary-text bg-white"></AiOutlineLeftCircle>{" "}
+            </Link>
+            <span>Back</span>
+          </div>
         </div>
       </div>
     </div>
