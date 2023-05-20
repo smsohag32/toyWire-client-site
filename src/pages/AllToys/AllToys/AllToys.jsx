860aecd30b70bc83dd29ac1cc130bed5d05aacdc
SM Sohag <sohagsheik32@gmail.com>
2023-05-20 11:03:50 +0600

    search bar and table implemented in all toys page

--- src/pages/AllToys/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys/AllToys.jsx
@@ -16,8 +16,85 @@ const AllToys = () => {
           </div>
         </div>
       </HeaderBanner>
-      <div className="toy-container">
-        <hr />
+      <div className="toy-container py-10">
+        <div>
+          <h2 className="text-center text-xl font-bold opacity-80">
+            Our Recommended All Robotic Toy Products
+          </h2>
+        </div>
+        <div className="mt-8 mb-1 md:max-w-sm mx-auto">
+          <form>
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search toys"
+              ></input>
+              <button
+                type="submit"
+                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="overflow-x-auto py-5">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Toy Name</th>
+                <th>Sub Category</th>
+                <th>Price</th>
+                <th>Available Quantity</th>
+                <th>View</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+              </tr>
+              {/* row 2 */}
+              <tr className="active">
+                <th>2</th>
+                <td>Hart Hagerty</td>
+                <td>Desktop Support Technician</td>
+                <td>Purple</td>
+              </tr>
+              {/* row 3 */}
+              <tr>
+                <th>3</th>
+                <td>Brice Swyre</td>
+                <td>Tax Accountant</td>
+                <td>Red</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
